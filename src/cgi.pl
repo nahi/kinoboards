701c4a8aa35de6425cd353321cd529a359195af2
nakahiro <nakahiro>
1997-02-06 17:28:02 +0000

    Added: SendMail for WinNT, Win95, and Mac. (only writing out to a file...)

--- src/cgi.pl
+++ src/cgi.pl
@@ -1,7 +1,7 @@
-# $Id: cgi.pl,v 1.12 1996-11-19 12:06:35 nakahiro Exp $
+# $Id: cgi.pl,v 1.13 1997-02-06 17:28:02 nakahiro Exp $
 
 
-# Small CGI tool package
+# Small CGI tool package(use this with jcode.pl-2.0).
 # Copyright (C) 1995, 96 NAKAMURA Hiroshi.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -24,16 +24,44 @@
 #
 package cgi;
 
-$MAIL2 = ((defined $'MAIL2) ? $'MAIL2 : "/usr/lib/sendmail -oi -t");
-$JPOUT_SCHEME = ((defined $'JPOUT_SCHEME) ? $'JPOUT_SCHEME : 'jis');
-$WAITPID_BLOCK = ((defined $'WAITPID_BLOCK) ? $'WAITPID_BLOCK : 0);
+require('jcode.pl');
+
+$ARCH = $main'ARCH;
+$MAIL2 = $main'MAIL2;
+$JPOUT_SCHEME = ($main'JPOUT_SCHEME || 'jis');
+$WAITPID_BLOCK = ($main'WAITPID_BLOCK || 0);
+
+@MONTH = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
+@WEEK_LABEL = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
 
 
 ###
 ## HTMLヘッダの生成
 #
 sub header {
-    print "Content-type: text/html\n\n";
+
+    local($Utc) = @_;
+
+    local($LastModified) = &GetHttpDateTimeFromUtc($Utc);
+
+    print(<<__EOF__);
+Content-type: text/html
+Last-Modified: $LastModified
+
+__EOF__
+
+}
+
+
+###
+## format as HTTP Date/Time
+#
+sub GetHttpDateTimeFromUtc {
+
+    local($Utc) = @_;
+    local($Sec, $Min, $Hour, $Mday, $Mon, $Year, $Wday, $Yday, $Isdst) = gmtime($Utc);
+    return(sprintf("%s, %02d-%s-%02d %02d:%02d:%02d GMT", $WEEK_LABEL[$Wday], $Mday, $MONTH[$Mon], $Year, $Hour, $Min, $Sec));
+
 }
 
 
@@ -43,7 +71,7 @@ sub header {
 #
 sub decode {
 
-    local($Args, $Nread, $Tag, $Term, $Value) = ('', '', '', '', '');
+    local($Args, $Nread, $Tag, $Term, $Value, $Code) = ();
 
     ($ENV{'REQUEST_METHOD'} eq "POST")
 	? ($Nread = read(STDIN, $Args, $ENV{'CONTENT_LENGTH'}))
@@ -53,14 +81,22 @@ sub decode {
 	($Tag, $Value) = split(/=/, $Term, 2);
 	$Value =~ tr/+/ /;
 	$Value =~ s/%([0-9A-Fa-f][0-9A-Fa-f])/pack("C", hex($1))/ge;
-	unless ($Value =~ /[\033\200-\377]/) {
+	$Code = &jcode'getcode(*Value); #'
+	if ($Code eq 'undef') {
 	    $TAGS{$Tag} = $Value;
 	} else {
-	    &jcode'convert(*Value, 'euc');
+	    &jcode'convert(*Value, 'euc', $Code, "z"); #'
 	    $TAGS{$Tag} = $Value;
 	}
-	$TAGS{$Tag} =~ s/\r\n/\n/go;
-	$TAGS{$Tag} =~ s/\r/\n/go;
+
+	if ($ARCH eq 'Mac') {
+	    $TAGS{$Tag} =~ s/\O15\O12/\n/go;
+	    $TAGS{$Tag} =~ s/\O15/\n/go;
+	} else {
+	    $TAGS{$Tag} =~ s/\r\n/\n/go;
+	    $TAGS{$Tag} =~ s/\r/\n/go;
+	}
+
     }
 }
 
@@ -92,15 +128,27 @@ sub KPrint {
 
 
 ###
-## メール送信(成功すると1，失敗すると0を返す)
+## メール送信
 #
 sub SendMail {
+
+    return(&SendMailSendmail(@_)) if ($ARCH eq 'UNIX');
+    return(&SendMailFile(@_)) if ($ARCH eq 'WinNT');
+    return(&SendMailFile(@_)) if ($ARCH eq 'Win95');
+    return(&SendMailFile(@_)) if ($ARCH eq 'Mac');
+
+}
+
+
+###
+## メール送信(UNIX用)
+#
+sub SendMailSendmail {
 
     # 送り主名前，送り主メイルアドレス，Subject，付加ヘッダ，
     # 引用記事(0なら無し)，宛先リスト
     # 本文以外には日本語を入れないように!
     local($FromName, $FromEmail, $Subject, $Extension, $Message, @To) = @_;
-
     local($Pid);
 
     # 安全のため，forkする
@@ -154,6 +202,67 @@ sub SendMail {
 
     # 送信した
     return(! $?);
+
+}
+
+
+###
+## メール送信(Mac, Win用)
+#
+sub SendMailFile {
+
+    # 送り主名前，送り主メイルアドレス，Subject，付加ヘッダ，
+    # 引用記事(0なら無し)，宛先リスト
+    # 本文以外には日本語を入れないように!
+    local($FromName, $FromEmail, $Subject, $Extension, $Message, @To) = @_;
+
+    local($ToFirst) = 1;
+
+    # メール用ファイルを開く
+    open(MAIL, ">> $MAIL2") || die;
+
+    # Toヘッダ
+    foreach (@To) {
+
+	if ($ToFirst) {
+	    print(MAIL "To: $_");
+	    $ToFirst = 0;
+	} else {
+	    print(MAIL ",\n\t$_");
+	}
+
+    }
+    print(MAIL "\n");
+    
+    # Fromヘッダ，Errors-Toヘッダ
+    $_ = "$FromName <$FromEmail>";
+    print(MAIL "From: $_\n");
+    print(MAIL "Errors-To: $_\n");
+
+    # Subjectヘッダ
+    print(MAIL "Subject: $Subject\n");
+
+    # 付加ヘッダ
+    if ($Extension) {
+	&jcode'convert(*Extension, 'jis'); #'
+	print(MAIL $Extension);
+    }
+
+    # ヘッダ終わり
+    print(MAIN "\n");
+
+    # 本文
+    &jcode'convert(*Message, 'jis'); #'
+    print(MAIL "$Message\n");
+
+    # 区切り線
+    print(MAIL "-------------------------------------------------------------------------------\n");
+
+    # 送信する
+    close(MAIL);
+
+    # 送信した
+    return(1);
 
 }
 
