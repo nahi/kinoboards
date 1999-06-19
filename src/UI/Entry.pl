760c2cda7a8c661d289b97a9048055f62583461a
nakahiro <nakahiro>
1999-06-19 11:44:34 +0000

    Changed form's layout. Notations moved the end of the page.

--- src/UI/Entry.pl
+++ src/UI/Entry.pl
@@ -126,80 +126,25 @@ __EOF__
 	}
     }
 
-    local( $msg ) = "<p>\n";
-
     local( $ttFlag ) = 0;
     local( $ttBit ) = 0;
+    
+    local( $note );
+    $note = "<p>\n";
     foreach ( @H_TTMSG )
     {
 	if (( $SYS_TEXTTYPE & ( 2**$ttBit )) &&
 	    ( $SYS_TEXTTYPE ^ ( 2** $ttBit )))
 	{
-	    $ttFlag = 1;
-	    $msg .= $H_TTMSG[$ttBit] . "\n";
+	    $ttFlag = 1;	# 後で使う．きたない．．．
+	    $note .= $H_TTMSG[$ttBit] . "\n";
 	}
 	$ttBit++;
     }
+    $note .= "</p>\n";
+    $note = '' unless $ttFlag;
 
-    $msg .= "</p>\n<p>\n$H_BOARD: $BOARDNAME<br>\n";
-
-    # アイコンの選択
-    if ( $SYS_ICON )
-    {
-	&CacheIconDb( $BOARD );	# アイコンDBをキャッシュ
-	$msg .= sprintf( "$H_ICON:\n<SELECT NAME=\"icon\">\n<OPTION%s>$H_NOICON\n", $DefIcon? '' : ' SELECTED' );
-	local( $IconTitle );
-	foreach $IconTitle ( @ICON_TITLE )
-	{
-	    $msg .= sprintf( "<OPTION%s>$IconTitle\n",
-		( $IconTitle eq $DefIcon )? ' SELECTED' : '' );
-	}
-	$msg .= "</SELECT>\n";
-
-	$msg .= "(" . &TagA( "$PROGRAM?b=$BOARD&c=i&type=entry",
-	    "アイコンの説明" ) . ")<BR>\n";
-    }
-
-    # Subject(フォローなら自動的に文字列を入れる)
-    $msg .= sprintf( "%s: <input name=\"subject\" type=\"text\" value=\"%s\" size=\"%s\"><br>\n", $H_SUBJECT, $DefSubject, $SUBJECT_LENGTH );
-
-    # TextType
-    if ( $ttFlag )
-    {
-	$ttFlag = 0 if $DefTextType;
-	$msg .= "$H_TEXTTYPE:\n<SELECT NAME=\"texttype\">\n";
-	$ttBit = 0;
-	foreach ( @H_TTLABEL )
-	{
-	    if ( $SYS_TEXTTYPE & ( 2 ** $ttBit ))
-	    {
-		if ( $ttFlag )
-		{
-		    $ttFlag = 0;	# now, using for a flag for the first.
-		    $msg .= "<OPTION SELECTED>" . $H_TTLABEL[$ttBit] . "\n";
-		}
-		else
-		{
-		    $msg .= sprintf( "<OPTION%s>" . $H_TTLABEL[$ttBit] . "\n",
-			( $H_TTLABEL[$ttBit] eq $DefTextType )? ' SELECTED' :
-			'' );
-		}
-	    }
-	    $ttBit++;
-	}
-	$msg .= "</SELECT>\n</p>\n";
-    }
-    else
-    {
-	$msg .= sprintf( "<input name=\"texttype\" type=\"hidden\" value=\"%s\">\n", $H_TTLABEL[(( log $SYS_TEXTTYPE ) / ( log 2 ))] );
-    }
-
-    # 本文
-    $msg .= "<p><textarea name=\"article\" rows=\"$TEXT_ROWS\" cols=\"$TEXT_COLS\">$DefArticle</textarea></p>\n";
-
-    # フッタ部分を表示
-    # 名前とメイルアドレス，URL．
-    $msg .=<<__EOF__;
+    $note .=<<__EOF__;
 <p>
 $H_MESG中に関連ウェブページへのリンクを張る場合は，
 「&lt;URL:http://〜&gt;」のように，URLを「&lt;URL:」と「&gt;」で囲んで
@@ -210,43 +155,100 @@ __EOF__
 
     if ( $SYS_F_S )
     {
-	$msg .= "この$H_BOARDの中の$H_MESGは\n";
-	$msg .= &TagA( "$PROGRAM?b=$BOARD&c=s", "キーワードで検索する" );
-	$msg .= "ことができます．\n";
+	$note .= "この$H_BOARDの中の$H_MESGは\n";
+	$note .= &TagA( "$PROGRAM?b=$BOARD&c=s", "キーワードで検索する" );
+	$note.= "ことができます．\n";
+    }
+    $note .= "</p>\n";
+
+    local( $msg );
+    # &TagForm()により，FORMタグの内側に<p>が自動付加されるため，
+    # ここではparagraphのstart tagをいれない．
+    $msg = '';
+
+    # アイコンの選択
+    if ( $SYS_ICON )
+    {
+	&CacheIconDb( $BOARD );	# アイコンDBをキャッシュ
+	$msg .= sprintf( "$H_ICON:\n<select name=\"icon\">\n<option%s>$H_NOICON\n", $DefIcon? '' : ' selected' );
+	local( $IconTitle );
+	foreach $IconTitle ( @ICON_TITLE )
+	{
+	    $msg .= sprintf( "<option%s>$IconTitle\n",
+		( $IconTitle eq $DefIcon )? ' selected' : '' );
+	}
+	$msg .= "</select>\n";
+
+	$msg .= "(" . &TagA( "$PROGRAM?b=$BOARD&c=i&type=entry",
+	    "使えるアイコン一覧" ) . ")<br>\n";
     }
 
-    $msg .= "</p>\n";
+    # Subject(フォローなら自動的に文字列を入れる)
+    $msg .= "$H_SUBJECT: <input name=\"subject\" type=\"text\" value=\"$DefSubject\" size=\"$SUBJECT_LENGTH\"><br>\n";
+
+    # 書き込み形式
+    if ( $ttFlag )
+    {
+	$ttFlag = 0 if $DefTextType;
+	$msg .= "$H_TEXTTYPE:\n<select name=\"texttype\">\n";
+	$ttBit = 0;
+	foreach ( @H_TTLABEL )
+	{
+	    if ( $SYS_TEXTTYPE & ( 2 ** $ttBit ))
+	    {
+		if ( $ttFlag )
+		{
+		    $ttFlag = 0;	# now, using for a flag for the first.
+		    $msg .= "<option selected>" . $H_TTLABEL[$ttBit] . "\n";
+		}
+		else
+		{
+		    $msg .= sprintf( "<option%s>" . $H_TTLABEL[$ttBit] . "\n",
+			( $H_TTLABEL[$ttBit] eq $DefTextType )? ' selected' :
+			'' );
+		}
+	    }
+	    $ttBit++;
+	}
+	$msg .= "</select>\n<br>\n";
+    }
+    else
+    {
+	$msg .= sprintf( "<input name=\"texttype\" type=\"hidden\" value=\"%s\">\n", $H_TTLABEL[(( log $SYS_TEXTTYPE ) / ( log 2 ))] );
+    }
+
+    # 本文
+    $msg .= "<textarea name=\"article\" rows=\"$TEXT_ROWS\" cols=\"$TEXT_COLS\">$DefArticle</textarea><br>\n";
 
     if ( $SYS_ALIAS == 0 )
     {
 	# エイリアスは使わない
 	$msg .=<<__EOF__;
-<p>
 $H_FROM: <input name="name" type="text" value="$DefName" size="$NAME_LENGTH"><br>
-$H_MAIL: <input name="mail" type="text" value="$DefEmail" size="$MAIL_LENGTH"><br>
 $H_URL_S:<br>
 <input name="url" type="text" value="$DefUrl" size="$URL_LENGTH"><br>
-</p>
+$H_MAIL: <input name="mail" type="text" value="$DefEmail" size="$MAIL_LENGTH"><br>
 __EOF__
     }
     elsif ( $SYS_ALIAS == 1 )
     {
 	# エイリアスを使う
-	$msg .= <<__EOF__;
-<p>
+	$msg .=<<__EOF__;
 $H_FROM: <input name="name" type="text" value="$DefName" size="$NAME_LENGTH"><br>
-$H_MAIL: <input name="mail" type="text" value="$DefEmail" size="$MAIL_LENGTH"><br>
 $H_URL_S:<br>
-<input name="url" type="text" value="$DefUrl" size="$URL_LENGTH">
-</p>
+<input name="url" type="text" value="$DefUrl" size="$URL_LENGTH"><br>
+$H_MAIL: <input name="mail" type="text" value="$DefEmail" size="$MAIL_LENGTH"><br>
+__EOF__
+
+	$note .=<<__EOF__;
 <p>
 「$H_ALIAS」に，$H_FROMと$H_MAIL，$H_URLを登録なさっている方は，
 「$H_FROM」に「#...」という登録名を書いてください．
 自動的に$H_FROMと$H_MAIL，$H_URLが補われます．
 __EOF__
 
-	$msg .= "(" . &TagA( "$PROGRAM?c=as", "$H_ALIASの一覧" ) . " // \n";
-	$msg .= &TagA( "$PROGRAM?c=an", "$H_ALIASを登録" ) . ")\n</p>\n";
+	$note .= "(" . &TagA( "$PROGRAM?c=as", "$H_ALIASの一覧" ) . " // \n";
+	$note .= &TagA( "$PROGRAM?c=an", "$H_ALIASを登録" ) . ")\n</p>\n";
     }
     elsif ( $SYS_ALIAS == 2 )
     {
@@ -256,28 +258,29 @@ __EOF__
 	&CacheAliasData;
 	&UnlockAll;
 	$msg .=<<__EOF__;
-<p>
 $H_USER:
-<SELECT NAME="name">
-<OPTION SELECTED>$H_FROMを登録した$H_ALIASを選んでください
+<select name="name">
+<option selected>$H_FROMを登録した$H_ALIASを選んでください
 __EOF__
 
 	local( $Key, $Value );
 	while (( $Key, $Value ) = each %Name )
 	{
-	    $msg .= "<OPTION>$Key\n";
+	    $msg .= "<option>$Key\n";
 	}
 	$msg .=<<__EOF__;
-</SELECT>
-</p>
+</select>
+__EOF__
+
+	$note .=<<__EOF__;
 <p>
 予め「$H_ALIAS」に，$H_FROMと$H_MAIL，$H_URLを登録しないと書き込めません．
 登録した後，「#...」という登録名を指定してください．
 __EOF__
 
-	$msg .= "(" . &TagA( "$PROGRAM?c=as", "$H_ALIASの一覧" ) . " // \n";
-	$msg .=&TagA( "$PROGRAM?c=an", "$H_ALIASを登録" ) . ")<br>\n";
-	$msg .=<<__EOF__;
+	$note .= "(" . &TagA( "$PROGRAM?c=as", "$H_ALIASの一覧" ) . " // \n";
+	$note .=&TagA( "$PROGRAM?c=an", "$H_ALIASを登録" ) . ")<br>\n";
+	$note .=<<__EOF__;
 登録した$H_ALIASが表示されない(選択できない)場合，
 このページを再読み込みしてください．
 </p>
@@ -287,16 +290,17 @@ __EOF__
     {
 	# HTTP-Cookiesを使う．
 	$msg .=<<__EOF__;
-<p>
 <input name="cookies" type="hidden" value="on">
 $H_FROM: <input name="name" type="text" value="$DefName" size="$NAME_LENGTH"><br>
-$H_MAIL: <input name="mail" type="text" value="$DefEmail" size="$MAIL_LENGTH"><br>
 $H_URL_S:<br>
-<input name="url" type="text" value="$DefUrl" size="$URL_LENGTH">
-</p>
+<input name="url" type="text" value="$DefUrl" size="$URL_LENGTH"><br>
+$H_MAIL: <input name="mail" type="text" value="$DefEmail" size="$MAIL_LENGTH"><br>
+__EOF__
+
+	$note .=<<__EOF__;
 <p>
 あなたのブラウザが「HTTP-Cookiesを使う」設定になっている場合，
-ここで指定した$H_FROM，$H_MAIL，$H_URLが
+上で入力した$H_FROM，$H_MAIL，$H_URLが
 あなたのブラウザ中に記憶されます．
 次回の書き込みの際は，その記憶された情報を利用できます．
 </p>
@@ -305,9 +309,8 @@ __EOF__
 
     if ( $SYS_MAIL & 2 )
     {
-	$msg .= sprintf( "<p>$H_REPLYがあった時にメイルで知らせますか? " .
-	    "<input name=\"fmail\" type=\"checkbox\" value=\"on\"%s></p>\n",
-	    $DefFmail? ' CHECKED' : '' );
+	$msg .= "この$H_MESGに$H_REPLYがあった時に，上の宛先にメイルで知らせますか? ";
+	$msg .= sprintf( "<input name=\"fmail\" type=\"checkbox\" value=\"on\"%s></p>\n", $DefFmail? ' CHECKED' : '' );
     }
     
     # ボタン
@@ -329,6 +332,10 @@ __EOF__
     %tags = ( 'corig', $COrig, 'b', $BOARD, 'c', 'p', 'id', $Id,
 	's', ( $entryType == 3 ), 'op', $op );
     &TagForm( *str, *tags, "実行", '', *msg );
+
+    # 注意書きいろいろ
+    $str .= "$H_HR\n$note" if $note;
+
     &cgiprint'Cache( $str );
 
     &MsgFooter;
