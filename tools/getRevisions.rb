7e364692db7ccd8608f1f6a2dc548872e7bd666b
nakahiro <nakahiro>
1999-09-25 04:11:08 +0000

    $_ is not set in in-line call back block now?

--- tools/getRevisions.rb
+++ tools/getRevisions.rb
@@ -16,12 +16,12 @@
 ARGV.each do |arg|
   file = ''
   revision = ''
-  open( "|cvs status #{arg}", "r" ).each do
-    if ( /^File: (\S*)\s*Status: (\S*)$/o )
+  open( "|cvs status #{arg}", "r" ).each do |line|
+    if ( line =~ /^File: (\S*)\s*Status: (\S*)$/o )
       raise "illegal file format. 'revision' not found." if file != ''
       raise "not Up-to-date: file" if ( $2 != 'Up-to-date' )
       file = $1
-    elsif ( /^   Repository revision:\s*(\S*)/ )
+    elsif ( line =~ /^   Repository revision:\s*(\S*)/ )
       raise "illegal file format. 'File' not found." if file == ''
       revision = $1
       print "#{file}: #{revision}\n"
